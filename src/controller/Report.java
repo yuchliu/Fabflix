7480a92330f263204a8b436fa616bdd06377c502
Jeremy <jjdubon@uci.edu>
2017-05-01 20:36:30 -0700

    Added un SQL and started the readme file.

---
+++ src/controller/Report.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.Movie;
+import service.MovieService;
+
+@WebServlet("/report/readme")
+public class Report extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/view/ReadMe.jsp").forward(request, response);
+    }
+
+}
---
+++ web/view/ReadMe.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>ReadMe</title>
+</head>
+<body>
+
+    <p>
+        To prepare the database:
+        <br />
+        Create a database called "moviedb" and run the createtable.sql file and the data.sql file against that database.
+        <br />
+        Create a user called "tomcat122b" with the password "rRQZtDDOYU3w" and give that user full privileges on moviedb.
+    </p>
+
+    <p>
+        To load the WAR file:
+        <br />
+        Junkai can you fill this out? I haven't been able to get the WAR file working on my machine.
+    </p>
+
+</body>
+</html>
